64f4d4f81010d9018802740f1e68ccb19945c628
n43635 <791219361@qq.com>
2018-06-03 20:05:26 +0800

    springboot的启动main

--- src/main/java/com/sx/springboot/fspringboot/FspringbootApplication.java
+++ src/main/java/com/sx/springboot/fspringboot/FspringbootApplication.java
@@ -3,10 +3,15 @@ package com.sx.springboot.fspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot的启动main
+ */
+
 @SpringBootApplication
 public class FspringbootApplication {
 
 	public static void main(String[] args) {
+//		启动了springboot程序,启动spring容器,启动内嵌的tomcat
 		SpringApplication.run(FspringbootApplication.class, args);
 	}
 }
