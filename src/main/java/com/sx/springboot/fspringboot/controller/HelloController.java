126e697e60a81e65c68bc0bad2debd95c3d00648
n43635 <791219361@qq.com>
2018-06-03 20:06:44 +0800

    springboot的Controller测试

---
+++ src/main/java/com/sx/springboot/fspringboot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.sx.springboot.fspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Spring的Controller测试
+ * 要注意与main同级目录或者在main的下级,在main上级会找不到
+ */
+
+@Controller
+public class HelloController {
+
+    //与Spring中的一样,相当于访问的子路径
+    @RequestMapping("/boot/hello")
+    public @ResponseBody String hello(){
+        return "Hello Spring Soot.";
+    }
+}
