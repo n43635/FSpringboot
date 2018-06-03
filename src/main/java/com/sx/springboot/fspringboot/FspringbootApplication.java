c6a1488aecde253eaa38b6a4fedd8b0141de825a
n43635 <791219361@qq.com>
2018-06-03 12:54:08 +0800

    Springboot测试练习

---
+++ src/main/java/com/sx/springboot/fspringboot/FspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.sx.springboot.fspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FspringbootApplication.class, args);
+	}
+}
